a4537549c95bfaf46bf41e1f13892c039350f691
Miguel SamaniegoTorres <samaniegotorres.ma@pg.com>
2025-02-19 09:02:57 +0100

    Another typo

--- eval.py
+++ eval.py
@@ -46,9 +46,9 @@ if __name__ == '__main__':
                 shutil.copy(test_images_dir / img_name, real_dir / img_name)
         # 4. Calculate the clean_fid between both folders
         fid_score = fid.compute_fid(str(fake_dir), str(real_dir))
-        print(f"FID for epoch {epoch}: {fid}")
+        print(f"FID for epoch {epoch}: {fid_score}")
         print(f"Calculating KID for epoch {epoch}")
         kid_score = fid.compute_kid(str(fake_dir), str(real_dir))
-        print(f"KID for epoch {epoch}: {fid}")
+        print(f"KID for epoch {epoch}: {kid_score}")
         # 5. Log results to wandb
         print("--------------------------------")
