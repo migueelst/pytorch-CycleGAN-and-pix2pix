caa8fbd0566c8644d362221ab3eeeddf3a5ec79f
Miguel SamaniegoTorres <samaniegotorres.ma@pg.com>
2025-03-10 12:56:44 +0100

    Add diff augment to pix2pix

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -1,6 +1,7 @@
 import torch
 from .base_model import BaseModel
 from . import networks
+from util.diff_augment import DiffAugment
 
 
 class Pix2PixModel(BaseModel):
@@ -91,11 +92,12 @@ class Pix2PixModel(BaseModel):
         """Calculate GAN loss for the discriminator"""
         # Fake; stop backprop to the generator by detaching fake_B
         fake_AB = torch.cat((self.real_A, self.fake_B), 1)  # we use conditional GANs; we need to feed both input and output to the discriminator
-        pred_fake = self.netD(fake_AB.detach())
+        policy = self.opt.diff_augment_policy
+        pred_fake = self.netD(fake_AB.detach()) if not policy else self.netD(DiffAugment(fake_AB.detach(), policy))
         self.loss_D_fake = self.criterionGAN(pred_fake, False)
         # Real
         real_AB = torch.cat((self.real_A, self.real_B), 1)
-        pred_real = self.netD(real_AB)
+        pred_real = self.netD(real_AB) if not policy else self.netD(DiffAugment(real_AB, policy))
         self.loss_D_real = self.criterionGAN(pred_real, True)
         # combine loss and calculate gradients
         self.loss_D = (self.loss_D_fake + self.loss_D_real) * 0.5
@@ -105,7 +107,8 @@ class Pix2PixModel(BaseModel):
         """Calculate GAN and L1 loss for the generator"""
         # First, G(A) should fake the discriminator
         fake_AB = torch.cat((self.real_A, self.fake_B), 1)
-        pred_fake = self.netD(fake_AB)
+        policy = self.opt.diff_augment_policy
+        pred_fake = self.netD(fake_AB) if not policy else self.netD(DiffAugment(fake_AB, policy))
         self.loss_G_GAN = self.criterionGAN(pred_fake, True)
         # Second, G(A) = B
         self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
